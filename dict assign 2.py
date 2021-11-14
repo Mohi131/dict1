672c9fca5067ed1b66b8f702df0a4ce770065a8d
Mohi131 <94045196+Mohi131@users.noreply.github.com>
2021-11-14 19:18:35 +0530

    Add files via upload

---
+++ dict assign 2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import string
+d = {c: ord(c) for c in string.ascii_lowercase}
+print(d)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
